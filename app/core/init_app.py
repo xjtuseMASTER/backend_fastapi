1e6d811ba54f4378f4c7da661fa4af48c304d7c9
Akane <cz1559428519@163.com>
2024-10-13 17:57:54 +0800

    feat: success run the testing interface

--- app/core/init_app.py
+++ app/core/init_app.py
@@ -182,22 +182,21 @@ async def init_menus():
 
 
 async def init_db():
-    pass
-    # command = Command(tortoise_config=settings.TORTOISE_ORM)
-    # try:
-    #     await command.init_db(safe=True)
-    # except FileExistsError:
-    #     pass
+    command = Command(tortoise_config=settings.TORTOISE_ORM)
+    try:
+        await command.init_db(safe=True)
+    except FileExistsError:
+        pass
 
-    # await command.init()
-    # try:
-    #     await command.migrate()
-    # except AttributeError:
-    #     logger.warning("unable to retrieve model history from database, model history will be created from scratch")
-    #     shutil.rmtree("migrations")
-    #     await command.init_db(safe=True)
+    await command.init()
+    try:
+        await command.migrate()
+    except AttributeError:
+        logger.warning("unable to retrieve model history from database, model history will be created from scratch")
+        shutil.rmtree("migrations")
+        await command.init_db(safe=True)
 
-    # await command.upgrade(run_in_transaction=True)
+    await command.upgrade(run_in_transaction=True)
 
 
 # async def init_roles():
